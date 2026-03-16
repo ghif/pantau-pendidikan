1f9fb8241f9e0e64f22ee3f5356dab2f6486c5fd
Muhammad Nassirudin <muhammad.nassirudin@teknologi.belajar.id>
2026-03-16 08:00:34 +0700

    fix: added true testimonies

--- src/data/testimonies.js
+++ src/data/testimonies.js
@@ -1,17 +1,17 @@
 export const TESTIMONIES = [
   {
-    testimony: "This service is highly recommended! The team was professional, prompt, and exceeded all my expectations. My conversion rates improved by nearly four times after implementing their solution.",
-    author: "Alex Johnson",
-    company: "Tech Solutions Inc.",
+    testimony: "As an education advocate involved in efforts to improve education across various regions, I find Pantau Pendidikan extremely helpful because it provides data visualizations and content that are ready to be communicated to the public about the state of education in a particular area. This makes advocacy with local governments, schools, and communities more targeted and more convincing in efforts to drive improvements in education.",
+    author: "Bukik",
+    company: "Head of <a href='https://yayasangurubelajar.org/?lang=en' target='_blank'>Guru Belajar Foundation</a> (Education Philanthropy)",
   },
   {
-    testimony: "NEVER BEEN EASIER!!!",
-    author: "Bianca Thompson",
-    company: "Education Observer",
+    testimony: "Pantau Pendidikan offers a much-needed breakthrough for education research in Indonesia. With limited high-quality education research, restricted access to data, and uneven data analysis capacity, this platform can help researchers produce new knowledge more effectively. It will be especially valuable for students, early-career researchers, policy researchers, and those working in government. By leveraging AI to improve access to and analysis of education data, Pantau Pendidikan can strengthen both the quantity and quality of research in Indonesia. If made publicly accessible and widely adopted, it has the potential to become a game changer for education policy research—supporting timely analysis and evidence-based recommendations on critical education issues.",
+    author: "Santoso",
+    company: "Education Researcher & Director of <a href='https://www.article33.or.id/en/tentang-article-33-indonesia/' target='_blank'>Article 33 Indonesia</a> (Policy Research Institution)",
   },
   {
-    testimony: "I know the quality of education my kids receive.",
-    author: "Carlo Santiago",
-    company: "Parent",
+    testimony: "I believe Pantau Pendidikan will help us transform complex education datasets into clear, actionable insights for practitioners and policymakers.  By making verified data accessible and easy to explore, the platform empowers us to make evidence-based decisions, identify local learning gaps, and design targeted interventions. Its user-friendly interface and AI supported analysis, bridge the gap between national statistics and on the ground practice, on top of that, it will also strengthen our ability to improve students’ learning outcomes across regions.",
+    author: "Stien",
+    company: "Head of <a href='https://www.instagram.com/lingkardaerahbelajar.id' target='_blank'>Lingkar Daerah Belajar</a> (Local Education Research Network)",
   },
 ]
